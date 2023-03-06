689364682d2a34e6aadc0eb1a6c2bb3a7eb98fa8
Woo Hyun (Ray) Hwang <rayhwang3130@gmail.com>
2023-03-06 22:43:45 +0900

    Update function_value.r

--- 2022 Summer Transfer Market Overview/function_value.r
+++ 2022 Summer Transfer Market Overview/function_value.r
@@ -7,13 +7,6 @@
 # 5. If value is free transfer, it should return 0.
 # 6. If value is loan transfer, it should return 0.
 
-# apply the function to market_value and transfer_value, and reassign the columns to the dataset we will be u9sing.
-
-# deleting the Pound sign
-selected$market_value <- gsub("€", "", selected$market_value)
-selected$transfer_value <- gsub("€", "", selected$transfer_value)
-selected$transfer_value
-
 # making a function to take care of m, Th., and others
 value <- function(list) {
   list[grep("*m", list)] <- as.integer(gsub("m", "", list[grep("*m", list)])) * 1000000
@@ -21,10 +14,3 @@ value <- function(list) {
   list[list %in% c('?', '-', 'loan transfer', 'free transfer', 'Loan fee:')] <- 0
   return(list)
 }
-
-# applying the function and changing the columns' format (non-scientific) and type (chr -> int)
-selected[, c("market_value", "transfer_value")] <- lapply(selected[, c("market_value", "transfer_value")], value)
-selected$market_value <- format(as.numeric(selected$market_value), scientific=FALSE)
-selected$transfer_value  <- format(as.numeric(selected$transfer_value), scientific=FALSE)
-selected[, c("market_value", "transfer_value")] <- lapply(selected[, c("market_value", "transfer_value")], as.integer)
-str(selected)
